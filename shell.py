89c10973f93793852e571907f40caa79b1187105
slowy07 <slowy.arfy@gmail.com>
2021-02-26 22:52:48 +0700

    update shell

--- shell.py
+++ shell.py
@@ -3,8 +3,8 @@ import basic
 
 print(basic.show_information)
 while True:
-    text = input(f'{basic.shell_running}')
-    result, error = basic.run('<stdin>', text)
+    text = input(f'{basic.shell_running[0]}')
+    result, error = basic.run(f'{basic.shell_running[1]}', text)
 
     if error: print(error.as_string())
     elif result: print(result)
