a53c53e97390e4c2290717a3770fd108aa0ae59a
slowy07 <slowy.arfy@gmail.com>
2021-02-28 20:02:25 +0700

    shell

--- shell.py
+++ shell.py
@@ -4,7 +4,13 @@ import basic
 print(basic.show_information)
 while True:
     text = input(f'{basic.shell_running[0]}')
+    
+    if text.strip() == "": continue
     result, error = basic.run(f'{basic.shell_running[1]}', text)
 
     if error: print(error.as_string())
-    elif result: print(result)
+    elif result: 
+        if len(result.elements) == 1:
+            print(repr(result.elements[0]))
+        else:
+            print(repr(result))
